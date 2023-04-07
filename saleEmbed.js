1bf68fd848d3357fccafb888a7a406fa82630a16
Akash <aakashrajp5@gmail.com>
2023-04-07 18:22:22 +0530

    fix

--- saleEmbed.js
+++ saleEmbed.js
@@ -25,7 +25,7 @@ let d = new Date(`${asset?.transaction?.timestamp}.000Z`);
   const dataEmbed = {
 	color: 0x4df85f,
 	title: '',
-	description: `[**${asset?.asset?.name} Sold!**](https://opensea.io/assets/ethereum/${contract_address}/${asset?.asset?.token_id})\n
+	description: `[**${asset?.asset?.name} Sold!**](https://opensea.io/assets/ethereum/0xc379e535caff250a01caa6c3724ed1359fe5c29b/${asset?.asset?.token_id})\n
     **Price: ${(parseInt(asset?.total_price)/(1000000000000000000)).toFixed(4)} ${asset?.payment_token?.symbol}** | $${((parseInt(asset?.total_price)/(1000000000000000000))*(parseInt(asset?.payment_token?.usd_price))).toFixed(2)} USD`,
 	thumbnail: {
 		url: asset?.asset?.image_url,
